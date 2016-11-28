66d0fc3aeba1f1269eea480404e1740a8acdbc50
Suimonkul <suimonkul311200@gmail.com>
2016-11-28 22:36:41 +0600

    Added mixing or randomizing adverts

--- app/src/main/java/appkg/kg/servicekg/activity/FavoriteActivity.java
+++ app/src/main/java/appkg/kg/servicekg/activity/FavoriteActivity.java
@@ -2,6 +2,8 @@ package appkg.kg.servicekg.activity;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Window;
+import android.view.WindowManager;
 
 import appkg.kg.servicekg.R;
 
@@ -13,6 +15,16 @@ public class FavoriteActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_favorite);
+        getWindow().addFlags(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(LAYOUT);
+        setTheme(STYLE);
+
+        initUI();
+    }
+
+    private void initUI() {
+
     }
 }
